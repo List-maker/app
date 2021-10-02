161f175122c08fe197d261b9cd79eced2c27fb69
Raphael <arabeyre@hotmail.fr>
2021-10-02 23:06:44 +0200

    create MorphIn widget

---
+++ lib/widgets/morph_in.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MorphIn extends StatelessWidget {
+  const MorphIn({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: child,); // TODO
+  }
+}
