ab916d4960cb4ce28a22dfd537f7d4ed0fe3aadf
Raphael <arabeyre@hotmail.fr>
2021-10-20 18:43:30 +0200

    Create safeScreen.dart

---
+++ lib/widgets/safeScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:list/style/theme.dart';
+
+class SafeScreen extends StatelessWidget {
+  const SafeScreen({Key? key, required this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: themeList.backgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.90,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
