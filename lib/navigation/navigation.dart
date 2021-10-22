fa2bfba8d9a91961d5bac2aca07f81779c0524fb
Raphael <arabeyre@hotmail.fr>
2021-10-22 11:37:40 +0200

    merge

---
+++ lib/navigation/navigation.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:list/navigation/startPage.dart';
+import 'package:list/screens/login.dart';
+import 'package:list/style/theme.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: themeList,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StartPage(),
+        '/login': (context) => Login(),
+        '/register': (context) => StartPage(),
+        '/hello': (context) => StartPage(),
+        '/home': (context) => StartPage(),
+        '/share': (context) => StartPage(),
+        '/profile': (context) => StartPage(),
+      },
+      // initialize all routes
+
+
+    );
+  }
+}
---
+++ lib/navigation/startPage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:list/call_db/token.dart';
+import 'package:list/model/token_model.dart';
+import 'package:list/style/ListIcons.dart';
+import 'package:list/style/theme.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  late Future<TokenModel> futureToken;
+
+  void goToNextPage() async{
+
+    await futureToken;
+
+    print('refresh_token');
+
+    await Future.delayed(Duration(seconds: 2));
+
+    Navigator.pushNamed(context, '/login');
+  }
+
+  @override
+  void initState() {
+    futureToken = getToken();
+    goToNextPage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: themeList.backgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Spacer(),
+              Icon(
+                ListIcons.check,
+                size: 100,
+                color: themeList.primaryColor,
+              ),
+              Text('List'),
+              SizedBox(
+                height: 250,
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
