0c3f39a6c95b431501a4e5f75514517e1cf09c10
Raphael <arabeyre@hotmail.fr>
2021-10-23 18:29:09 +0200

    Create alert.dart

---
+++ lib/widgets/alert.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Alert extends StatelessWidget {
+  const Alert({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
