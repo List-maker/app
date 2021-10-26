b9c83aaecca941c2909b13927cf25d5977a4655f
Raphael <arabeyre@hotmail.fr>
2021-10-26 19:33:53 +0200

    create table user

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ void main() async{
     join(await getDatabasesPath(), 'database.db'),
     onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE token(id INTEGER PRIMARY KEY, refresh_token TEXT, access_token TEXT)',
+        'CREATE TABLE token(id INTEGER PRIMARY KEY, refresh_token TEXT, access_token TEXT); CREATE TABLE user(id INTEGER PRIMARY KEY, username TEXT,	email TEXT, pinned_lists TEXT, settings TEXT);',
+      //TODO : pined list c'est pas un text
       );
     },
     version: 1,
