bce3d59f11293457f2760951bdff88d4077b468e
Raphael <arabeyre@hotmail.fr>
2021-11-08 19:14:47 +0100

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:list/api/listCalls.dart';
+import 'package:list/widgets/listWidget.dart';
 import 'package:list/widgets/safeScreen.dart';
 
 class Home extends StatefulWidget {
@@ -9,12 +11,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late Future<List> futureListsId;
+
+  test() async{
+
+    print( fetchListsId());
+  }
+
+  @override
+  void initState() {
+    futureListsId = fetchListsId();
+    test();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeScreen(
       title: "Home",
       child: Center(
-        child: Text("data"),
+        child: FutureBuilder<List>(
+          future: futureListsId,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListWidget(id: snapshot.data!.elementAt(index));
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    );
+                  },
+                ),
+              );
+            }
+            return Text("data");
+          },
+        ),
       ),
     );
   }
