fa8f93d29df432793ea00a676efb06c1f49a931c
Raphael <arabeyre@hotmail.fr>
2021-11-12 22:48:02 +0100

    Update itemWidget.dart

--- lib/widgets/itemWidget.dart
+++ lib/widgets/itemWidget.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:list/api/itemCalls.dart';
+import 'package:list/model/item_model.dart';
+
+import 'morphIn.dart';
+import 'morphOut.dart';
 
 class ItemWidget extends StatefulWidget {
-  const ItemWidget({Key? key,required this.id}) : super(key: key);
+  const ItemWidget({Key? key, required this.id}) : super(key: key);
   final int id;
 
   @override
-  _ItemWidgetState createState() => _ItemWidgetState(id : id);
+  _ItemWidgetState createState() => _ItemWidgetState(id: id);
 }
 
 class _ItemWidgetState extends State<ItemWidget> {
   _ItemWidgetState({required this.id});
+
+  late Future<ItemModel> futureItem;
+
   final int id;
 
   @override
+  void initState() {
+    futureItem = fetchItem(id);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text(id.toString());
+    return FutureBuilder<ItemModel>(
+      future: futureItem,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          ItemModel item = snapshot.data!;
+          if (item.checked) {
+            return MorphIn(
+              child: Text(item.name),
+            );
+          } else {
+            return MorphOut(
+              child: Text(item.name),
+            );
+          }
+        }
+        return Text("data");
+      },
+    );
   }
 }
