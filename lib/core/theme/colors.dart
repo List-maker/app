91522e003279d9bfc7ad0000d6c4add41066177b
Raphael Arabeyre <raphael.arabeyre@ynov.com>
2022-03-14 19:34:38 +0100

    fix: rename color and add error color

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -2,7 +2,9 @@ import 'dart:ui';
 
 const primary = Color(0xFF482FF7);
 const background = Color(0xFFD9D7E3);
-const whiteText = Color(0xFFFFFFFF);
+const white = Color(0xFFFFFFFF);
 const gray = Color(0xFF7F7F7F);
 
-final primarySelection = Color(0x33482FF7);
+const primarySelection = Color(0x33482FF7);
+
+const error = Color(0xFFFF2B2B);
