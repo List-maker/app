05ff2467b318a1a9ab70dc76003769a64b95352c
Raphael Arabeyre <raphael.arabeyre@ynov.com>
2022-03-23 11:45:09 +0100

    fix: delete unused hello module

--- lib/app/modules/home/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:list/app/modules/hello/controller.dart';
-
-class HelloBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<HelloController>(() => HelloController());
-  }
-}
